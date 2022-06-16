1e79c3e44263a5be169cdf2c0a9f9cd79986ea92
test_git <shj1478@naver.com>
2022-06-16 15:25:59 +0900

    [ADD] song

--- GitForkTestProject/src/TestMainProject.java
+++ GitForkTestProject/src/TestMainProject.java
@@ -3,6 +3,7 @@ public class TestMainProject {
 
 	public static void main(String[] args) {
 		System.out.println("main");
+		
 	}
 
 }
---
+++ GitForkTestProject/src/song/Songjob.java
@@ -0,0 +1,7 @@
+package song;
+
+
+
+public class Songjob {
+
+}
